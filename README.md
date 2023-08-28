297f86d8bb1cebeae2f878ffa282e423090073a2
Guilherme-DAngelo <guigui.dangelo@icloud.com>
2023-08-28 19:26:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Validação de Formulário com JavaScript
+
+## Visão geral
+
+Este é um desafio do módulo de front-end do curso DevQuest do canal Dev em Dobro. O objetivo deste desafio é realizar a validação de um formulário através do JavaScript com as seguintes exigências:
+
+- A validação do formulário deve ser feita com
+javascript puro.
+- Ao clicar para enviar o formulário, se caso
+algum campo não estiver preenchido, a borda
+do input deve ficar vermelha e uma mensagem
+de "campo obrigatório" deve aparecer embaixo
+do campo que não foi preenchido, conforme o
+figma.
+- O fundo do formulário deve ser feito usando a
+imagem em anexo na aula.
+
+### Resultado
+
+![Imagem](src/images/projeto.gif)
+<<<<<<< HEAD
+
+[Formulário de Validação com JS](https://guilherme-dangelo.github.io/formulario-com-js-intermediario/)
+=======
+>>>>>>> 4932a7b2bfe5d34a4430d6186a25030301f5d708
+
+## Meu processo
+
+### Feito com:
+
+- Marcação semântica HTML
+- CSS
+- JavaScript
+
+
+### O que eu aprendi:
+
+Com esse desafio, eu aprendi a importância de se ter um HTML guiado á objetos que seja organizado e simples de entender,  também aprendi a trabalhar melhor com as resoluções de diferentes dispositivos e por consequência, melhorei a responsividade da minha página web, utilizando CSS.
+
+## Autor
+
+- Github - [Guilherme D'Angelo](https://github.com/Guilherme-DAngelo)
+- Linkedin - [Guilherme D'Angelo](https://www.linkedin.com/in/guilherme-d-655705218/)
---
+++ src/js/validation.js
@@ -0,0 +1,122 @@
+const erroNome = document.getElementById('aviso-nome')
+const erroEmail = document.getElementById('aviso-email')
+const erroTelefone = document.getElementById('aviso-telefone')
+const erroMensagem = document.getElementById('aviso-mensagem')
+
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    validarNome()
+    validarEmail()
+    validarTelefone()
+    validarMensagem()
+    
+})
+
+let exibirMensagemErroNome = (mensagem) =>{
+    erroNome.textContent = mensagem;
+    erroNome.classList.add('erro');
+
+}
+let esconderMensagemErroNome = function() {
+    erroNome.classList.remove('erro');
+}
+
+function validarNome() {
+    const nome = document.querySelector('.nome')
+    const input = document.querySelector('#nome')
+    const adicionarClaseErro = nome.classList.add('erro')
+
+    if(input.value.trim() === ""){
+        adicionarClaseErro
+        exibirMensagemErroNome('campo obrigatorio')
+        nome.classList.remove('sucesso')
+    }else if(input.value !== "") {
+        nome.classList.remove('erro')
+        nome.classList.add('sucesso')
+        esconderMensagemErroNome()
+    }
+}
+
+const exibirMensagemErroEmail = (mensagem) => {
+    erroEmail.textContent = mensagem
+    erroEmail.classList.add('erro');
+}
+
+const esconderMensagemErroEmail = function() {
+    erroEmail.classList.remove('erro');
+}
+
+function validarEmail () {
+    const email = document.querySelector('.email')
+    const meuEmail = document.getElementById('email')
+    meuEmail.addEventListener('blur', validarEmail);
+
+    if(meuEmail.value.trim() === ""){
+        email.classList.add('erro')
+        exibirMensagemErroEmail('campo obrigatorio')
+        email.classList.remove('sucesso')
+    }else if(!isValidEmail(meuEmail.value) !== "") {
+        email.classList.remove('erro')
+        email.classList.add('sucesso')
+        esconderMensagemErroEmail()
+    }
+}
+
+function isValidEmail(email) { 
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+
+let exibirMensagemErroTelefone = (mensagem) => {
+    erroTelefone.textContent = mensagem;
+    erroTelefone.classList.add('erro');
+}
+
+const esconderMensagemErroTelefone = function() {
+    erroTelefone.classList.remove('erro');
+}
+
+function validarTelefone() {
+    const telefone = document.querySelector('.telefone')
+    const numeroTelefone = document.getElementById('telefone')
+
+    if(numeroTelefone.value.trim() === "") {
+        telefone.classList.add('erro')
+        exibirMensagemErroTelefone('campo obrigatorio')
+        telefone.classList.remove('sucesso')
+    }else if(numeroTelefone.value.trim() !== ""){  
+        telefone.classList.remove('erro')
+        telefone.classList.add('sucesso')
+        esconderMensagemErroTelefone()
+    }
+
+}
+
+const exibirMensagemErroMensagem = (mensagem) => {
+    erroMensagem.textContent = mensagem;
+    erroMensagem.classList.add('erro');
+}
+
+let esconderMensagemErroMensagem = function() {
+    erroMensagem.classList.remove('erro');
+}
+
+function validarMensagem() {
+    const mensagem = document.querySelector('.msg')
+    const campoMensagem = document.getElementById('mensagem')
+
+    if (campoMensagem.value.trim() === "") {
+        mensagem.classList.add('erro')
+        exibirMensagemErroMensagem('campo obrigatorio')
+        mensagem.classList.remove('sucesso')
+    }else if (campoMensagem.value.trim() !== ""){
+        mensagem.classList.remove('erro')
+        mensagem.classList.add('sucesso')
+        esconderMensagemErroMensagem()
+    }
+}
